7c72d089fef4c86b999bdc26ee9fac315f003435
NasifTazwar <nasiftazwar@gmail.com>
2024-11-17 15:28:50 +0600

    class-2

---
+++ Class 2/acp.py
@@ -0,0 +1,7 @@
+name1 = input("Enter your birthday, Student 1: ")
+name2 = input("Enter your birthday, Student 2: ")
+name3 = input("Enter your birthday, Student 3: ")
+
+print("name 1 birthday is on ", name1)
+print("name 2 birthday is on ", name2)
+print("name 3 birthday is on ", name3)
